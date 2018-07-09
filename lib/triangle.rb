b8e7ba71f94510a566c61c9475a3915fe0e0c0d2
Joana Palero <joana.palero24@gmail.com>
2018-07-09 21:15:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,16 @@ class Triangle
     @s3 = s3
   end
   
-  
+  def kind 
+    validate_triangle
+    if s1 == s2 && s2 == s3
+      :equilateral
+      elsif s1 == s2 || s2 == s3 || s1 == s3
+      :isosceles
+    else
+      :scalene
+    end
+  end
   
   
   class TriangleError < StandardError
